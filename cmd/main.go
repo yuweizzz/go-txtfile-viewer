03b79c1288971e0eb5ae3f23063b0822a2372753
yuweizzz <yuwei764969238@gmail.com>
2023-09-25 11:16:03 +0800

    feat: base server enhance

--- cmd/main.go
+++ cmd/main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"log"
-	
 	"net/http"
+
 	"github.com/yuweizzz/go-txtfile-viewer/pkg"
 )
 
 func main() {
 	fsys := pkg.CustomFileSystem{http.Dir("/tmp/text_files")}
-	http.Handle("/", http.FileServer(fsys))
+	http.Handle("/", pkg.CustomFileServer(fsys))
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- pkg/filesystem.go
+++ pkg/filesystem.go
@@ -1,24 +1,32 @@
 package pkg
 
 import (
+	"bytes"
+	"fmt"
+	"io"
 	"io/fs"
 	"net/http"
+	"net/url"
+	"sort"
 	"strings"
+	"time"
 )
 
-func confirmTxtfile(name string) bool {
-	return strings.HasSuffix(name, ".txt")
-}
-
-type txtFile struct {
+type TxtFile struct {
 	http.File
 }
 
-func (f txtFile) Readdir(n int) (fis []fs.FileInfo, err error) {
+func (f TxtFile) Readdir(n int) (fis []fs.FileInfo, err error) {
 	files, err := f.File.Readdir(n)
 	for _, file := range files {
-		if confirmTxtfile(file.Name()) {
-			fis = append(fis, file)
+		if !strings.HasPrefix(file.Name(), ".") {
+			if file.IsDir() {
+				fis = append(fis, file)
+				continue
+			}
+			if strings.HasSuffix(file.Name(), ".txt") {
+				fis = append(fis, file)
+			}
 		}
 	}
 	return
@@ -33,5 +41,108 @@ func (fsys CustomFileSystem) Open(name string) (http.File, error) {
 	if err != nil {
 		return nil, err
 	}
-	return txtFile{file}, err
+	return TxtFile{file}, err
+}
+
+func CustomFileServer(root CustomFileSystem) http.Handler {
+
+	return &TxtFileHandler{root}
+}
+
+type TxtFileHandler struct {
+	root CustomFileSystem
+}
+
+func (f *TxtFileHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	upath := r.URL.Path
+	if !strings.HasPrefix(upath, "/") {
+		upath = "/" + upath
+		r.URL.Path = upath
+	}
+
+	file, err := f.root.Open(upath)
+	if err != nil {
+		io.WriteString(w, "404 page not found1")
+		w.WriteHeader(404)
+		return
+	}
+	if stat, _ := file.Stat(); !stat.IsDir() {
+		contentBytes, err := io.ReadAll(file)
+		if err != nil {
+			io.WriteString(w, err.Error())
+			w.WriteHeader(500)
+			return
+		}
+
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		http.ServeContent(w, r, upath, time.Now(), bytes.NewReader(contentBytes))
+		return
+	}
+	dirList(w, r, TxtFile{file})
+}
+
+type anyDirs interface {
+	len() int
+	name(i int) string
+	isDir(i int) bool
+}
+
+type fileInfoDirs []fs.FileInfo
+
+func (d fileInfoDirs) len() int          { return len(d) }
+func (d fileInfoDirs) isDir(i int) bool  { return d[i].IsDir() }
+func (d fileInfoDirs) name(i int) string { return d[i].Name() }
+
+type dirEntryDirs []fs.DirEntry
+
+func (d dirEntryDirs) len() int          { return len(d) }
+func (d dirEntryDirs) isDir(i int) bool  { return d[i].IsDir() }
+func (d dirEntryDirs) name(i int) string { return d[i].Name() }
+
+var htmlReplacer = strings.NewReplacer(
+	"&", "&amp;",
+	"<", "&lt;",
+	">", "&gt;",
+	// "&#34;" is shorter than "&quot;".
+	`"`, "&#34;",
+	// "&#39;" is shorter than "&apos;" and apos was not in HTML until HTML5.
+	"'", "&#39;",
+)
+
+func dirList(w http.ResponseWriter, r *http.Request, f http.File) {
+	// Prefer to use ReadDir instead of Readdir,
+	// because the former doesn't require calling
+	// Stat on every entry of a directory on Unix.
+	var dirs anyDirs
+	var err error
+	if d, ok := f.(fs.ReadDirFile); ok {
+		var list dirEntryDirs
+		list, err = d.ReadDir(-1)
+		dirs = list
+	} else {
+		var list fileInfoDirs
+		list, err = f.Readdir(-1)
+		dirs = list
+	}
+
+	if err != nil {
+		http.Error(w, "Error reading directory", http.StatusInternalServerError)
+		return
+	}
+	sort.Slice(dirs, func(i, j int) bool { return dirs.name(i) < dirs.name(j) })
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprintf(w, "<pre>\n")
+	for i, n := 0, dirs.len(); i < n; i++ {
+		name := dirs.name(i)
+		if dirs.isDir(i) {
+			name += "/"
+		}
+		// name may contain '?' or '#', which must be escaped to remain
+		// part of the URL path, and not indicate the start of a query
+		// string or fragment.
+		url := url.URL{Path: name}
+		fmt.Fprintf(w, "<a href=\"%s\">%s</a>\n", url.String(), htmlReplacer.Replace(name))
+	}
+	fmt.Fprintf(w, "</pre>\n")
 }
